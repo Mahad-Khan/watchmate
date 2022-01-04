ceff246572df2f406dd25bbb25bb1da1f586b8c6
Mahad-Khan <mahadkhan.ubit@gmail.com>
2022-01-04 15:53:11 +0500

    add stream create test

--- watchmate/user_app/tests.py
+++ watchmate/user_app/tests.py
@@ -16,3 +16,26 @@ class RegisterTestCase(APITestCase):
             }
         response = self.client.post(reverse('register'), data, format='json')
         self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+
+
+class LoginLogoutTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='example101', password='Pass@123')
+
+    def test_login(self):
+        """ test that login successful return status code """
+        data ={
+            "username": "example101",
+            "password": "Pass@123"
+        }
+        response = self.client.post(reverse('login'), data, format='json')
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+    def test_logout(self):
+        """ test that logout successful and return status code """
+        self.token = Token.objects.get(user__username='example101')
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key) # carrying a token
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+
--- watchmate/watchlist_app/tests.py
+++ watchmate/watchlist_app/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
 
 # Create your tests here.
+class StreamPlatformTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_superuser(
+            username='foobar',
+            email='foo@bar.com',
+            password='barbaz')
+        # self.user = User.objects.create(username='foobar', password='Pass@123')
+        # self.user.is_staff = True
+        self.token = Token.objects.get(user__username='foobar')
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+    def test_stream_platform_create(self):
+        "test that only admin can create platform and return status"
+        data ={
+            "name":"Netflix",
+            "about":"Lumber one Streaming Platform",
+            "website":"https://www.netflix.com/"
+        }
+        response = self.client.post(reverse('streamplatform-list'), data, format='json')
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
